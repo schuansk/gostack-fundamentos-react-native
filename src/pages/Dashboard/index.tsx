de8d56902ac23688ec18371dc3243165a179b59c
schuansk <schuansk@hotmail.com.br>
2020-12-24 11:20:26 -0300

    :sparkles: challenge finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,16 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const responde = await api.get('/products');
+
+      setProducts(responde.data);
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
